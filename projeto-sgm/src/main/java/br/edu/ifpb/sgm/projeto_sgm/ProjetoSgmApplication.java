908acaee296465738039d213354228941a2fe4c1
Pedro Vinicius <159270159+vinPedro@users.noreply.github.com>
2025-05-26 14:56:51 -0300

    Primeiro Commit

---
+++ projeto-sgm/src/main/java/br/edu/ifpb/sgm/projeto_sgm/ProjetoSgmApplication.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.sgm.projeto_sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSgmApplication.class, args);
+		//System.out.println("Hello Wolrd");
+
+	}
+
+}
---
+++ projeto-sgm/src/test/java/br/edu/ifpb/sgm/projeto_sgm/ProjetoSgmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.sgm.projeto_sgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
