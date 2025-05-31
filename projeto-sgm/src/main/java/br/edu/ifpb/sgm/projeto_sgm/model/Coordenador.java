545bba05d4871272a29a2d226111995bdc4855d8
Davidsousaaz <david.sousagit0048@gmail.com>
2025-05-31 13:34:28 -0300

    fix: corrige entidade Coordenador

---
+++ projeto-sgm/src/main/java/br/edu/ifpb/sgm/projeto_sgm/model/Coordenador.java
@@ -0,0 +1,23 @@
+package br.edu.ifpb.sgm.projeto_sgm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "coordenador")
+public class Coordenador extends Professor {
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
+}
